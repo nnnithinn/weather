7e45976f0b4838951d101212bc6152fa471045e2
nnnithinn <nnnithinn24@gmail.com>
2023-05-09 09:33:18 +0400

    first commit

---
+++ src/main/java/com/cyphase/weatherfetch/WeatherfetchApplication.java
@@ -0,0 +1,13 @@
+package com.cyphase.weatherfetch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherfetchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherfetchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cyphase/weatherfetch/WeatherfetchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyphase.weatherfetch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherfetchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
